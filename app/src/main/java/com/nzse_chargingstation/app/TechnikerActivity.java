0a2c2b458edd0ba10e31cf1c051967792c1ffb0a
Agustinus_Nicander Hery <nicanderhery@gmail.com>
2022-05-29 15:33:59 +0200

    added back button in TechnikerActivity

--- app/src/main/java/com/nzse_chargingstation/app/TechnikerActivity.java
+++ app/src/main/java/com/nzse_chargingstation/app/TechnikerActivity.java
@@ -3,12 +3,19 @@ package com.nzse_chargingstation.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TechnikerActivity extends AppCompatActivity {
+
+    Button btn_back_techniker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_techniker);
+
+        btn_back_techniker = findViewById(R.id.button_back_techniker);
+
+        btn_back_techniker.setOnClickListener(v -> finish());
     }
 }
