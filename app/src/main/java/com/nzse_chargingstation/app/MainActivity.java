75d15ee76d8fd3e1dcc560e27e753581d0127b32
Agustinus_Nicander Hery <nicanderhery@gmail.com>
2022-05-30 02:43:46 +0200

    added get my current location

--- app/src/main/java/com/nzse_chargingstation/app/MainActivity.java
+++ app/src/main/java/com/nzse_chargingstation/app/MainActivity.java
@@ -1,12 +1,19 @@
 package com.nzse_chargingstation.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,9 +24,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
     BottomNavigationView bottom_nav_bar;
+    private GoogleMap map;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in {@link
+     * #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean permissionDenied = false;
+
+
 
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -76,17 +103,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // mMap.setMaxZoomPreference(4);
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+//        map.setMaxZoomPreference(4);
+
+        map = googleMap;
+        map.setOnMyLocationButtonClickListener(this);
+        map.setOnMyLocationClickListener(this);
+        enableMyLocation();
 
         LatLng department = new LatLng (49.86625273516996, 8.640257820411557);
-        googleMap.addMarker(new MarkerOptions().position(department).title("Marker h_da, fbi"));
+        map.addMarker(new MarkerOptions().position(department).title("Marker h_da, fbi"));
 
         LatLng kfc = new LatLng (49.905710, 8.581990);
-        googleMap.addMarker(new MarkerOptions().position(department).title("KFC Weiterstadt"));
+        map.addMarker(new MarkerOptions().position(kfc).title("KFC Weiterstadt"));
 
         LatLng mediacampus  = new LatLng (49.902004957188076, 8.854893065467536);
-        googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).position(mediacampus).title("Marker Media Campus"));
+        map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).position(mediacampus).title("Marker Media Campus"));
 /*
         Here are the approximate zoom levels and what they do :
         1: World
@@ -95,13 +127,83 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         15: Streets
         20: Buildings
  */
-        float zoomLevel = (float) 17.0;
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(department, zoomLevel));
+        float zoomLevel = (float) 10.0;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(department, zoomLevel));
 
 //        double centerlat = (mediacampus.latitude+department.latitude)/2;
 //        double centerlon = (mediacampus.longitude+department.longitude)/2;
 //        LatLng center  = new LatLng (centerlat, centerlon);
 //        googleMap.animateCamera(CameraUpdateFactory.newLatLng(department));
 //        googleMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    private void enableMyLocation() {
+        // 1. Check if permissions are granted, if so, enable the my location layer
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+            return;
+        }
+
+        // 2. Otherwise, request location permissions from the user.
+        PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE, Manifest.permission.ACCESS_COARSE_LOCATION, true);
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION) || PermissionUtils
+                .isPermissionGranted(permissions, grantResults,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Permission was denied. Display an error message
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true;
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            permissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
