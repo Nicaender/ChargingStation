de1a80cea2cbd4213a07d053c2672595ffbc6b96
Agustinus_Nicander Hery <nicanderhery@gmail.com>
2022-06-08 11:54:05 +0200

    fixed defective distance comparator

--- app/src/main/java/com/nzse_chargingstation/app/classes/DefectiveDistanceComparator.java
+++ app/src/main/java/com/nzse_chargingstation/app/classes/DefectiveDistanceComparator.java
@@ -5,7 +5,21 @@ import java.util.Comparator;
 public class DefectiveDistanceComparator implements Comparator<Defective> {
     public int compare(Defective df1, Defective df2)
     {
-        double distance = (ContainerAndGlobal.calculateLength(df1.getDefectiveCs().getLocation(), ContainerAndGlobal.getCurrentLocation()) - ContainerAndGlobal.calculateLength(df2.getDefectiveCs().getLocation(), ContainerAndGlobal.getCurrentLocation()));
+        double d1, d2;
+        if(df1.getDefectiveCs() != null)
+        {
+            d1 = ContainerAndGlobal.calculateLength(df1.getDefectiveCs().getLocation(), ContainerAndGlobal.getCurrentLocation());
+        }
+        else
+            d1 = ContainerAndGlobal.calculateLength(df1.getDefectiveFavorite().getFavoriteCs().getLocation(), ContainerAndGlobal.getCurrentLocation());
+        if(df2.getDefectiveCs() != null)
+        {
+            d2 = ContainerAndGlobal.calculateLength(df2.getDefectiveCs().getLocation(), ContainerAndGlobal.getCurrentLocation());
+        }
+        else
+            d2 = ContainerAndGlobal.calculateLength(df2.getDefectiveFavorite().getFavoriteCs().getLocation(), ContainerAndGlobal.getCurrentLocation());
+
+        double distance = d1 - d2;
         if(distance < -0.00001)
             return -1;
         if(distance > 0.00001)
